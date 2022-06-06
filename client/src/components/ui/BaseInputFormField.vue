ce508e72d7662b0bebaee21908a9e87da1ba3d20
vy.ct <vyct@mor.com.vn>
2022-06-06 19:06:10 +0900

    two ways binding value from formComponent

--- client/src/components/ui/BaseInputFormField.vue
+++ client/src/components/ui/BaseInputFormField.vue
@@ -4,7 +4,8 @@
             {{ label }}
         </label>
         <input :type="type" :name="name"
-            class="h-9 w-full px-[0.375rem] py-3 rounded-lg bg-gray-50 border-[1px] border-solid border-gray-200 " />
+            class="h-9 w-full px-[0.375rem] py-3 rounded-lg bg-gray-50 border-[1px] border-solid border-gray-200 "
+            @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" :value="modelValue" />
     </div>
 </template>
 
@@ -14,7 +15,8 @@ import { defineProps, InputHTMLAttributes, } from 'vue';
 interface IBaseFormInputField {
     label: string;
     type: InputHTMLAttributes['type'];
-    name: string
+    name: string;
+    modelValue: string;
 }
 
 defineProps<IBaseFormInputField>();
--- client/src/views/AuthView.vue
+++ client/src/views/AuthView.vue
@@ -1,15 +1,23 @@
 <template>
     <section class="grid items-center min-h-screen">
-        <form autocomplete="off"
+        <form autocomplete="off" @submit.prevent="handleSubmit"
             class="max-w-[400px] w-[90vw] mx-auto my-12 px-8 py-10 bg-white rounded-lg border-t-[5px] border-t-cyan-500 transition duration-300 ease-in shadow-lg hover:shadow-2xl">
             <LogoSVG class="h-[50px] block mx-auto mt-0 mb-5" />
-            <h3 class="text-3xl text-center mb-5">Login</h3>
-            <BaseInputFormField label="Email" type="email" name="email" />
-            <BaseInputFormField label="Password" type="password" name="password" />
+            <h3 class="text-3xl text-center mb-5">
+                {{ isRegisterMode ? 'Register' : 'Login' }}
+            </h3>
+            <BaseInputFormField label="Name" type="name" name="name" v-model="nameInput" v-show="isRegisterMode" />
+            <BaseInputFormField label="Email" type="email" name="email" v-model="emailInput" />
+            <BaseInputFormField label="Password" type="password" name="password" v-model="passwordInput" />
             <BaseButton type="submit" class="mt-8">
-                Login
+                {{ isRegisterMode ? 'Register' : 'Login' }}
             </BaseButton>
-
+            <p class="text-center text-lg mt-4 mx-0 font-semibold">
+                {{ isRegisterMode ? 'Already a member?' : 'Not a member yet?' }}
+                <button type="button" @click="toggleMode" class="text-cyan-500 font-bold ">
+                    {{ isRegisterMode ? 'Login' : 'Register' }}
+                </button>
+            </p>
         </form>
     </section>
 </template>
@@ -18,5 +26,26 @@
 import LogoSVG from '@/assets/images/logo.svg'
 import BaseInputFormField from '@/components/ui/BaseInputFormField.vue';
 import BaseButton from '@/components/ui/BaseButton.vue';
+import { ref } from 'vue';
+
+const nameInput = ref<string>('');
+const emailInput = ref<string>('');
+const passwordInput = ref<string>('');
+
+const isRegisterMode = ref<boolean>(false);
+const toggleMode = () => {
+    isRegisterMode.value = !isRegisterMode.value
+}
+
+const handleSubmit = () => {
+    const formValue = {
+        name: nameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value,
+    }
+
+    console.log(formValue);
+}
+
 
 </script>
