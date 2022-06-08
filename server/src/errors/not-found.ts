0aa3188d47eed7a6ebefda01a300e47cb8e2dc83
vy.ct <vyct@mor.com.vn>
2022-06-08 12:25:34 +0900

    set up error

---
+++ server/src/errors/not-found.ts
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+import CustomAPIError from "./custom-api-error";
+
+class NotFoundError extends CustomAPIError {
+    status: number;
+    constructor(message: string) {
+        super(message)
+        this.status = StatusCodes.NOT_FOUND
+    }
+}
