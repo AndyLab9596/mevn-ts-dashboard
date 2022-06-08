2167b3face19bc1fe2e41ebd474fc21667e77e7f
vy.ct <vyct@mor.com.vn>
2022-06-08 11:52:37 +0900

    create error page

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
-        <h3>Not Found</h3>
-    </div>
+    <main class="min-h-screen grid place-items-center text-center">
+        <div>
+            <NotFoundSVG />
+            <h3 class="text-3xl mb-2 font-semibold">Ohh! Page Not Found</h3>
+            <p class="text-gray-500 text-lg my-4">We can't seem to find the page you're looking for</p>
+            <RouterLink :to="{ name: 'home' }" class="text-cyan-500 underline decoration-cyan-500 capitalize p-1">
+                Back Home
+            </RouterLink>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
+import NotFoundSVG from '@/assets/images/not-found.svg';
 
 </script>
-
-<style scoped>
-
-</style>
