a72741f18c7531604da2e4b325878c4df1a22fb0
vy.ct <vyct@mor.com.vn>
2022-06-08 17:39:24 +0900

    authentication function and setup interceptor

---
+++ client/src/models/userTypes.ts
@@ -0,0 +1,18 @@
+export interface ILoginUserPayload {
+    email: string;
+    password: string;
+}
+
+export interface IRegisterUserPayload extends ILoginUserPayload {
+    name: string;
+}
+
+export interface IUpdateUser extends Omit<IRegisterUserPayload, 'password'> {
+    location: string;
+    lastName: string;
+}
+export interface IUserInfo {
+    user: IUpdateUser;
+    token: string;
+    location: string
+}
